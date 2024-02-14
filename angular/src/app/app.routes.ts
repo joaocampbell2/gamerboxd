d42ef7a8fe2135222a5f07f25c38ec28006e82eb
João Campbell <jpcampbell2012@hotmail.com>
2024-02-14 16:57:38 -0300

    toolbar + cores + routing

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", redirectTo: "games", pathMatch: "full"},
+  {
+    path: 'games',
+    loadChildren: () => import('./games/games.module').then(m => m.GamesModule)
+  }
+];
