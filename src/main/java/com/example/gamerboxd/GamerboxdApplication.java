5227e38f5e310142f261d1c213843f0a7718671f
João Campbell <jpcampbell2012@hotmail.com>
2024-02-14 14:02:17 -0300

    first commit

---
+++ src/main/java/com/example/gamerboxd/GamerboxdApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamerboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerboxdApplication.class, args);
+	}
+
+}
